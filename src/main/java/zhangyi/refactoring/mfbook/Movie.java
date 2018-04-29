e90e570ecc881ae65ce18badab54ffdf61300d24
iamzhangyi <15023157626@163.com>
2018-04-29 16:35:43 +0800

    yi - extract method for each branch of if statement‘

--- src/main/java/zhangyi/refactoring/mfbook/Movie.java
+++ src/main/java/zhangyi/refactoring/mfbook/Movie.java
@@ -29,19 +29,34 @@ public class Movie {
         double thisAmount = 0;
         switch (this.getPriceCode()) {
             case REGULAR:
-                thisAmount += 2;
-                if (daysRented > 2)
-                    thisAmount += (daysRented - 2) * 1.5;
+                thisAmount = amountForRegular(daysRented, thisAmount);
                 break;
             case NEW_RELEASE:
-                thisAmount += daysRented * 3;
+                thisAmount = amountForNewRelease(daysRented, thisAmount);
                 break;
             case CHILDREN:
-                thisAmount += 1.5;
-                if (daysRented > 3)
-                    thisAmount += (daysRented - 3) * 1.5;
+                thisAmount = amountForChildren(daysRented, thisAmount);
                 break;
         }
+        return thisAmount;
+    }
+
+    private double amountForChildren(int daysRented, double thisAmount) {
+        thisAmount += 1.5;
+        if (daysRented > 3)
+            thisAmount += (daysRented - 3) * 1.5;
+        return thisAmount;
+    }
+
+    private double amountForNewRelease(int daysRented, double thisAmount) {
+        thisAmount += daysRented * 3;
+        return thisAmount;
+    }
+
+    private double amountForRegular(int daysRented, double thisAmount) {
+        thisAmount += 2;
+        if (daysRented > 2)
+            thisAmount += (daysRented - 2) * 1.5;
         return thisAmount;
     }
 }

9642aec8da1bdb2d31f99ed18782033785297d46
iamzhangyi <15023157626@163.com>
2018-04-29 10:14:54 +0800

    yi - extract methods using composed method pattern

--- src/main/java/zhangyi/cleancode/composemethods/PartDB.java
+++ src/main/java/zhangyi/cleancode/composemethods/PartDB.java
@@ -1,9 +1,6 @@
 package zhangyi.cleancode.composemethods;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,21 +13,33 @@ public class PartDB {
     private List<Part> partList = new ArrayList<Part>();
 
     public void populate() throws Exception {
-        Connection c = null;
+        Connection connection = null;
         try {
-            Class.forName(DRIVER_CLASS);
-            c = DriverManager.getConnection(DB_URL, USER, PASSWORD);
-            Statement stmt = c.createStatement();
-            ResultSet rs = stmt.executeQuery(SQL_SELECT_PARTS);
+            connection = getConnection();
+            ResultSet rs = getResultSet(connection);
             while (rs.next()) {
-                Part p = new Part();
-                p.setName(rs.getString("name"));
-                p.setBrand(rs.getString("brand"));
-                p.setRetailPrice(rs.getDouble("retail_price"));
-                partList.add(p);
+                populateParts(rs);
             }
         } finally {
-            c.close();
+            connection.close();
         }
+    }
+
+    private void populateParts(ResultSet rs) throws SQLException {
+        Part p = new Part();
+        p.setName(rs.getString("name"));
+        p.setBrand(rs.getString("brand"));
+        p.setRetailPrice(rs.getDouble("retail_price"));
+        partList.add(p);
+    }
+
+    private ResultSet getResultSet(Connection connection) throws SQLException {
+        Statement stmt = connection.createStatement();
+        return stmt.executeQuery(SQL_SELECT_PARTS);
+    }
+
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName(DRIVER_CLASS);
+        return DriverManager.getConnection(DB_URL, USER, PASSWORD);
     }
 }

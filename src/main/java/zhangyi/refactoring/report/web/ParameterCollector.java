390c8b1b23fd45444d3eea6d02fe76b9ce7af89d
iamzhangyi <15023157626@163.com>
2018-04-29 10:41:47 +0800

    yi - extract methods for each branch of if statement

--- src/main/java/zhangyi/refactoring/report/web/ParameterCollector.java
+++ src/main/java/zhangyi/refactoring/report/web/ParameterCollector.java
@@ -7,37 +7,49 @@ public class ParameterCollector {
     public void fillParameters(ServletHttpRequest request, ParameterGraph parameterGraph) {
         for (Parameter para : parameterGraph.getParmaeters()) {
             if (para instanceof SimpleParameter) {
-                SimpleParameter simplePara = (SimpleParameter) para;
-                String[] values = request.getParameterValues(simplePara.getName());
-                simplePara.setValue(values);
+                fillSimpleParameter(request, (SimpleParameter) para);
             } else {
                 if (para instanceof ItemParameter) {
-                    ItemParameter itemPara = (ItemParameter) para;
-                    for (Item item : itemPara.getItems()) {
-                        String[] values = request.getParameterValues(item.getName());
-                        item.setValues(values);
-                    }
+                    fillItemParameter(request, (ItemParameter) para);
                 } else {
-                    TableParameter tablePara = (TableParameter) para;
-                    String[] rows =
-                            request.getParameterValues(tablePara.getRowName());
-                    String[] columns =
-                            request.getParameterValues(tablePara.getColumnName());
-                    String[] dataCells =
-                            request.getParameterValues(tablePara.getDataCellName());
-
-                    int columnSize = columns.length;
-                    for (int i = 0; i < rows.length; i++) {
-                        for (int j = 0; j < columns.length; j++) {
-                            TableParameterElement element = new TableParameterElement();
-                            element.setRow(rows[i]);
-                            element.setColumn(columns[j]);
-                            element.setDataCell(dataCells[columnSize * i + j]);
-                            tablePara.addElement(element);
-                        }
-                    }
+                    fillTableParameter(request, (TableParameter) para);
                 }
             }
         }
+    }
+
+    private void fillTableParameter(ServletHttpRequest request, TableParameter para) {
+        TableParameter tablePara = para;
+        String[] rows =
+                request.getParameterValues(tablePara.getRowName());
+        String[] columns =
+                request.getParameterValues(tablePara.getColumnName());
+        String[] dataCells =
+                request.getParameterValues(tablePara.getDataCellName());
+
+        int columnSize = columns.length;
+        for (int i = 0; i < rows.length; i++) {
+            for (int j = 0; j < columns.length; j++) {
+                TableParameterElement element = new TableParameterElement();
+                element.setRow(rows[i]);
+                element.setColumn(columns[j]);
+                element.setDataCell(dataCells[columnSize * i + j]);
+                tablePara.addElement(element);
+            }
+        }
+    }
+
+    private void fillItemParameter(ServletHttpRequest request, ItemParameter para) {
+        ItemParameter itemPara = para;
+        for (Item item : itemPara.getItems()) {
+            String[] values = request.getParameterValues(item.getName());
+            item.setValues(values);
+        }
+    }
+
+    private void fillSimpleParameter(ServletHttpRequest request, SimpleParameter para) {
+        SimpleParameter simplePara = para;
+        String[] values = request.getParameterValues(simplePara.getName());
+        simplePara.setValue(values);
     }
 }

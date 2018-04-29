9236fac62a4dda9af004d32a7e96b24994533e49
iamzhangyi <15023157626@163.com>
2018-04-29 10:04:59 +0800

    yi- extract method for paperboy

--- src/main/java/zhangyi/refactoring/demeter/Paperboy.java
+++ src/main/java/zhangyi/refactoring/demeter/Paperboy.java
@@ -2,6 +2,10 @@ package zhangyi.refactoring.demeter;
 
 public class Paperboy {
     public void charge(Customer myCustomer, float payment) {
+        pay(myCustomer, payment);
+    }
+
+    private void pay(Customer myCustomer, float payment) {
         Wallet theWallet = myCustomer.getWallet();
         if (theWallet.getTotalMoney() > payment) {
             theWallet.subtractMoney(payment);

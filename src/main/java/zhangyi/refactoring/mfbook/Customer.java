12e1f4a073cd4d5e6a4d85b5afd74eea2bd56cae
agiledon <15023157626@163.com>
2019-08-05 20:14:30 +0800

    extract methods for calculation logic

--- src/main/java/zhangyi/refactoring/mfbook/Customer.java
+++ src/main/java/zhangyi/refactoring/mfbook/Customer.java
@@ -20,28 +20,38 @@ class Customer {
     }
 
     public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
         String result = "Rental Record for " + getName() + "\n";
 
-        for (Rental rental : rentals) {
-            frequentRenterPoints += rental.pointsFor();
-        }
+        int frequentRenterPoints = totalPoints();
 
         for (Rental rental : rentals) {
             //show figures
             result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(rental.amountFor()) + "\n";
         }
 
-        for (Rental rental : rentals) {
-            totalAmount += rental.amountFor();
-        }
+        double totalAmount = totalAmounts();
 
         //add footer lines
         result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints) +
                 " frequent renter points";
         return result;
+    }
+
+    private double totalAmounts() {
+        double totalAmount = 0;
+        for (Rental rental : rentals) {
+            totalAmount += rental.amountFor();
+        }
+        return totalAmount;
+    }
+
+    private int totalPoints() {
+        int frequentRenterPoints = 0;
+        for (Rental rental : rentals) {
+            frequentRenterPoints += rental.pointsFor();
+        }
+        return frequentRenterPoints;
     }
 
 }

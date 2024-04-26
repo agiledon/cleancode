d6900e3bbbdebf5938777c93576951eda96e39d1
zhangyi <brucezhang@tom.com>
2024-04-26 17:32:11 +0800

    yi - write the unit tests for Receipt

---
+++ src/test/java/zhangyi/refactoring/extractmethod/ReceiptTest.java
@@ -0,0 +1,31 @@
+package zhangyi.refactoring.extractmethod;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ReceiptTest {
+    @Test
+    public void calculate_grand_total_without_discount() {
+        Receipt receipt = new Receipt();
+        receipt.addItemTotal(400d);
+        receipt.addItemTotal(600d);
+
+        double grandTotal = receipt.calculateGrandTotal();
+
+        assertThat(grandTotal).isEqualTo(1065d);
+    }
+
+    @Test
+    public void calculate_grand_total_with_discount() {
+        Receipt receipt = new Receipt();
+        receipt.addItemTotal(400d);
+        receipt.addItemTotal(600d);
+        receipt.addItemTotal(200d);
+        receipt.addDiscount(200d);
+
+        double grandTotal = receipt.calculateGrandTotal();
+
+        assertThat(grandTotal).isEqualTo(1065d);
+    }
+}

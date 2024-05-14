3e8420287a631e416bdc0d63d4c3941fc03278ea
zhangyi <brucezhang@tom.com>
2024-05-14 17:07:58 +0800

    yi - chapter 6 - delete switch statement and apply demeter law

--- src/main/java/zhangyi/refactoring/report/engine/ParameterGraph.java
+++ src/main/java/zhangyi/refactoring/report/engine/ParameterGraph.java
@@ -1,8 +1,15 @@
 package zhangyi.refactoring.report.engine;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 public class ParameterGraph {
+    public void fillValues(HttpServletRequest request) {
+        for (Parameter para : getParameters()) {
+            para.fill(request);
+        }
+    }
+
     public List<Parameter> getParameters() {
         return null;
     }
--- src/main/java/zhangyi/refactoring/report/web/ParameterController.java
+++ src/main/java/zhangyi/refactoring/report/web/ParameterController.java
@@ -6,17 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 
 public class ParameterController {
     public void fillParameters(HttpServletRequest request, ParameterGraph parameterGraph) {
-        for (Parameter para : parameterGraph.getParameters()) {
-            if (para instanceof SimpleParameter) {
-                para.fill(request);
-            } else {
-                if (para instanceof ItemParameter) {
-                    para.fill(request);
-                } else {
-                    para.fill(request);
-                }
-            }
-        }
+        parameterGraph.fillValues(request);
     }
 
 }

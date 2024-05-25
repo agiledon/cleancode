7b8ec0ca26c6d49e1cebb7ed82140d16d09521c3
zhangyi <brucezhang@tom.com>
2024-05-25 10:14:30 +0800

    yi - chapter 7 - rename variables and parameters to make sense

--- src/main/java/zhangyi/cleancode/fitness/HtmlUtil.java
+++ src/main/java/zhangyi/cleancode/fitness/HtmlUtil.java
@@ -1,56 +1,58 @@
 package zhangyi.cleancode.fitness;
 
 public class HtmlUtil {
-    public static String testableHtml(PageData pageData, boolean includeSuiteSetup) throws Exception {
+    public static String testableHtml(PageData pageData, boolean isSuite) throws Exception {
         if (isTestPage(pageData)) {
-            StringBuffer buffer = new StringBuffer();
+            StringBuffer newPageContent = new StringBuffer();
             WikiPage wikiPage = pageData.getWikiPage();
 
-            if (includeSuiteSetup) {
-                includeSuiteSetup(wikiPage, buffer);
+            if (isSuite) {
+                includeSuiteSetup(wikiPage, newPageContent);
             }
-            includeSetup(wikiPage, buffer);
-            includeTestContent(pageData, buffer);
-            includeTeardown(wikiPage, buffer);
-            if (includeSuiteSetup) {
-                includeSuiteTeardown(wikiPage, buffer);
+            includeSetup(wikiPage, newPageContent);
+            includeTestContent(pageData, newPageContent);
+            includeTeardown(wikiPage, newPageContent);
+            if (isSuite) {
+                includeSuiteTeardown(wikiPage, newPageContent);
             }
 
-            pageData.setContent(buffer.toString());
+            pageData.setContent(newPageContent.toString());
         }
         return pageData.getHtml();
     }
 
-    private static void includeSuiteSetup(WikiPage wikiPage, StringBuffer buffer) {
-        includeInheritedPage(SuiteResponder.SUITE_SETUP_NAME, wikiPage, buffer, "\n!include -setup .");
+    private static void includeSuiteSetup(WikiPage wikiPage, StringBuffer newPageContent) {
+        includeInheritedPage(wikiPage, newPageContent, SuiteResponder.SUITE_SETUP_NAME, "-setup");
     }
 
-    private static void includeSetup(WikiPage wikiPage, StringBuffer buffer) {
-        includeInheritedPage("SetUp", wikiPage, buffer, "\n!include -setup .");
+    private static void includeSetup(WikiPage wikiPage, StringBuffer newPageContent) {
+        includeInheritedPage(wikiPage, newPageContent, "SetUp", "-setup");
     }
 
-    private static StringBuffer includeTestContent(PageData pageData, StringBuffer buffer) {
-        return buffer.append(pageData.getContent());
+    private static StringBuffer includeTestContent(PageData pageData, StringBuffer newPageContent) {
+        return newPageContent.append(pageData.getContent());
     }
 
-    private static void includeTeardown(WikiPage wikiPage, StringBuffer buffer) {
-        includeInheritedPage("TearDown", wikiPage, buffer, "\n!include -teardown .");
+    private static void includeTeardown(WikiPage wikiPage, StringBuffer newPageContent) {
+        includeInheritedPage(wikiPage, newPageContent, "TearDown", "-teardown");
     }
 
-    private static void includeSuiteTeardown(WikiPage wikiPage, StringBuffer buffer) {
-        includeInheritedPage(SuiteResponder.SUITE_TEARDOWN_NAME, wikiPage, buffer, "\n!include -teardown .");
+    private static void includeSuiteTeardown(WikiPage wikiPage, StringBuffer newPageContent) {
+        includeInheritedPage(wikiPage, newPageContent, SuiteResponder.SUITE_TEARDOWN_NAME, "-teardown");
     }
 
     private static boolean isTestPage(PageData pageData) {
         return pageData.hasAttribute("Test");
     }
 
-    private static void includeInheritedPage(String inheritedPageName, WikiPage wikiPage, StringBuffer buffer, String pageName) {
+    private static void includeInheritedPage(WikiPage wikiPage, StringBuffer newPageContent, String inheritedPageName, String pageName) {
         WikiPage inheritedPage = PageCrawlerImpl.getInheritedPage(inheritedPageName, wikiPage);
         if (inheritedPage != null) {
             WikiPagePath pagePath = inheritedPage.getPageCrawler().getFullPath(inheritedPage);
             String pagePathName = PathParser.render(pagePath);
-            buffer.append(pageName)
+            newPageContent.append("\n!include ")
+                    .append(pageName)
+                    .append(" .")
                     .append(pagePathName)
                     .append("\n");
         }

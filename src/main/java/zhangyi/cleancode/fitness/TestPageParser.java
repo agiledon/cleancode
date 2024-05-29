d42b0628ec8e3d17bb9b20bd271d116260c19ef4
zhangyi <brucezhang@tom.com>
2024-05-29 17:58:00 +0800

    yi - chapter 8 - remove some parameters

--- src/main/java/zhangyi/cleancode/fitness/TestPageParser.java
+++ src/main/java/zhangyi/cleancode/fitness/TestPageParser.java
@@ -2,66 +2,66 @@ package zhangyi.cleancode.fitness;
 
 public class TestPageParser {
 
-    private PageData pageData;
+    private final StringBuffer newPageContent;
+    private final WikiPage wikiPage;
+    private final PageData pageData;
 
     public TestPageParser(PageData pageData) {
         this.pageData = pageData;
+        newPageContent = new StringBuffer();
+        wikiPage = this.pageData.getWikiPage();
     }
 
-    static void includeSuiteSetup(WikiPage wikiPage, StringBuffer newPageContent) {
-        includeInheritedPage(wikiPage, newPageContent, SuiteResponder.SUITE_SETUP_NAME, "-setup");
+    public String parse(boolean isSuite) {
+        if (isTestPage()) {
+            if (isSuite) {
+                includeSuiteSetup();
+            }
+            includeSetup();
+            includeTestContent();
+            includeTeardown();
+            if (isSuite) {
+                includeSuiteTeardown();
+            }
+            this.pageData.setContent(newPageContent.toString());
+        }
+        return this.pageData.getHtml();
     }
 
-    static void includeSetup(WikiPage wikiPage, StringBuffer newPageContent) {
-        includeInheritedPage(wikiPage, newPageContent, "SetUp", "-setup");
+    private void includeSuiteSetup() {
+        includeInheritedPage(SuiteResponder.SUITE_SETUP_NAME, "-setup");
     }
 
-    static StringBuffer includeTestContent(PageData pageData, StringBuffer newPageContent) {
-        return newPageContent.append(pageData.getContent());
+    private void includeSetup() {
+        includeInheritedPage("SetUp", "-setup");
     }
 
-    static void includeTeardown(WikiPage wikiPage, StringBuffer newPageContent) {
-        includeInheritedPage(wikiPage, newPageContent, "TearDown", "-teardown");
+    private void includeTestContent() {
+        this.newPageContent.append(this.pageData.getContent());
     }
 
-    static void includeSuiteTeardown(WikiPage wikiPage, StringBuffer newPageContent) {
-        includeInheritedPage(wikiPage, newPageContent, SuiteResponder.SUITE_TEARDOWN_NAME, "-teardown");
+    private void includeTeardown() {
+        includeInheritedPage("TearDown", "-teardown");
     }
 
-    static boolean isTestPage(PageData pageData) {
-        return pageData.hasAttribute("Test");
+    private void includeSuiteTeardown() {
+        includeInheritedPage(SuiteResponder.SUITE_TEARDOWN_NAME, "-teardown");
     }
 
-    static void includeInheritedPage(WikiPage wikiPage, StringBuffer newPageContent, String inheritedPageName, String pageName) {
-        WikiPage inheritedPage = PageCrawlerImpl.getInheritedPage(inheritedPageName, wikiPage);
+    private boolean isTestPage() {
+        return this.pageData.hasAttribute("Test");
+    }
+
+    private void includeInheritedPage(String inheritedPageName, String pageName) {
+        WikiPage inheritedPage = PageCrawlerImpl.getInheritedPage(inheritedPageName, this.wikiPage);
         if (inheritedPage != null) {
             WikiPagePath pagePath = inheritedPage.getPageCrawler().getFullPath(inheritedPage);
             String pagePathName = PathParser.render(pagePath);
-            newPageContent.append("\n!include ")
+            this.newPageContent.append("\n!include ")
                     .append(pageName)
                     .append(" .")
                     .append(pagePathName)
                     .append("\n");
         }
-    }
-
-    public String parse(boolean isSuite) {
-        if (isTestPage(this.pageData)) {
-            StringBuffer newPageContent = new StringBuffer();
-            WikiPage wikiPage = this.pageData.getWikiPage();
-
-            if (isSuite) {
-                includeSuiteSetup(wikiPage, newPageContent);
-            }
-            includeSetup(wikiPage, newPageContent);
-            includeTestContent(this.pageData, newPageContent);
-            includeTeardown(wikiPage, newPageContent);
-            if (isSuite) {
-                includeSuiteTeardown(wikiPage, newPageContent);
-            }
-
-            this.pageData.setContent(newPageContent.toString());
-        }
-        return this.pageData.getHtml();
     }
 }

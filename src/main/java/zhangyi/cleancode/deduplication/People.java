3bdb09acb4fbebc17bae77059b10cfe488c504c7
zhangyi <brucezhang@tom.com>
2024-08-03 09:05:49 +0800

    yi - chapter 14 - use predicate

--- src/main/java/zhangyi/cleancode/deduplication/People.java
+++ src/main/java/zhangyi/cleancode/deduplication/People.java
@@ -2,17 +2,27 @@ package zhangyi.cleancode.deduplication;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class People {
     private List<Person> persons = new ArrayList<Person>();
 
     public List<Person> findByName(String name) {
-        List<Person> result = new ArrayList<Person>();
-        for (Person person : persons) {
-            if (person.getName().equals(name)) {
-                result.add(person);
-            }
-        }
-        return result;
+        return findBy(p -> p.getName().equals(name));
     }
+
+    public List<Person> findByAge(int age) {
+        return findBy(p -> p.getAge() == age);
+    }
+
+    public List<Person> findByGender(Gender gender) {
+        return findBy(p -> p.getGender().equals(gender));
+    }
+
+    private List<Person> findBy(Predicate<Person> predicate) {
+        return persons.stream().filter(predicate).collect(Collectors.toList());
+    }
+
+
 }

a8f8195cb9788a7a75cc9e9dce051373bfc89955
zhangyi <brucezhang@tom.com>
2024-09-07 09:32:18 +0800

    yi - chapter 17 - add the demo for assertJ

--- src/test/java/zhangyi/framework/junit/StandardTest.java
+++ src/test/java/zhangyi/framework/junit/StandardTest.java
@@ -3,43 +3,49 @@ package zhangyi.framework.junit;
 import org.junit.jupiter.api.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.fail;
+import static org.assertj.core.api.Fail.fail;
 
 public class StandardTest {
-    public StandardTest() {
-        System.out.println("this is a constructor.");
-    }
-
     @BeforeAll
     static void initAll() {
-        System.out.println("before all");
+        System.out.println("before all / init all");
     }
+
     @BeforeEach
     void init() {
-        System.out.println("before");
+        System.out.println("before each / init");
     }
+
     @Test
     void succeedingTest() {
         System.out.println("succeeding test");
-        assertThat(true).isTrue();
+
+        String str = "assertJ provides Fluent assertions for java";
+        assertThat(str).startsWith("assertJ")
+                .endsWith("java")
+                .containsIgnoringCase("fluent");
     }
+
     @Test
     @Disabled("skip the failure test")
     void failingTest() {
         System.out.println("failing test");
         fail("a failing test");
     }
+
     @Test
     @Disabled("for demonstration purposes")
     void skippedTest() {
         System.out.println("skipped test");
     }
+
     @AfterEach
     void tearDown() {
-        System.out.println("after each");
+        System.out.println("after each / tear down");
     }
+
     @AfterAll
     static void tearDownAll() {
-        System.out.println("after all");
+        System.out.println("after all / tear down all");
     }
 }

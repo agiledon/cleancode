44298ceeee2d31f971c59a6de62a97514a06f064
zhangyi <brucezhang@tom.com>
2024-10-16 12:34:21 +0800

    yi - chapter 19 - refactor current test method

--- src/test/java/zhangyi/refactoring/codegen/service/impl/ApplicationServiceImplTest.java
+++ src/test/java/zhangyi/refactoring/codegen/service/impl/ApplicationServiceImplTest.java
@@ -14,20 +14,8 @@ class ApplicationServiceImplTest {
     @Test
     public void should_generate_parent_pom_file_by_ProjectFileWriter() {
         // given
-        ApplicationMetadata applicationMetadata = new ApplicationMetadata();
+        ApplicationMetadata applicationMetadata = createApplicationMetadata();
 
-        String projectName = "demo3-parent";
-        applicationMetadata.setName(projectName);
-
-        String projectGroup = "com.xm";
-        String projectArtifact = "demo1-parent";
-        String projectVersion = "1.0-SNAPSHOT";
-        ProjectMavenCoordinate appMavenCoordinate = new ProjectMavenCoordinate(projectGroup, projectArtifact, projectVersion);
-
-        applicationMetadata.setProject(appMavenCoordinate);
-        applicationMetadata.setModules(Arrays.asList("module01", "module02", "module03", "module04"));
-
-        String templateName = "parent-pom.ftl";
         String fileName = "demo1-parent/pom.xml";
         ProjectFileWriter mockedWriter = mock(ProjectFileWriter.class);
         when(mockedWriter.write(eq(applicationMetadata), anyString())).thenReturn(fileName);
@@ -40,5 +28,21 @@ class ApplicationServiceImplTest {
         // then
         verify(mockedWriter).write(eq(applicationMetadata), anyString());
         assertThat(generatedFileName).isEqualTo(fileName);
+    }
+
+    private static ApplicationMetadata createApplicationMetadata() {
+        ApplicationMetadata applicationMetadata = new ApplicationMetadata();
+
+        String projectName = "demo3-parent";
+        applicationMetadata.setName(projectName);
+
+        String projectGroup = "com.xm";
+        String projectArtifact = "demo1-parent";
+        String projectVersion = "1.0-SNAPSHOT";
+        ProjectMavenCoordinate appMavenCoordinate = new ProjectMavenCoordinate(projectGroup, projectArtifact, projectVersion);
+
+        applicationMetadata.setProject(appMavenCoordinate);
+        applicationMetadata.setModules(Arrays.asList("module01", "module02", "module03", "module04"));
+        return applicationMetadata;
     }
 }

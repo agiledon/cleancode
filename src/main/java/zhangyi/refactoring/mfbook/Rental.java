462da059da9e27e3bd77fa6d46e2e1ada0997b98
zhangyi <brucezhang@tom.com>
2024-11-30 12:18:42 +0800

    yi - chapter 22 - inline getMovie and getDaysRented

--- src/main/java/zhangyi/refactoring/mfbook/Rental.java
+++ src/main/java/zhangyi/refactoring/mfbook/Rental.java
@@ -10,46 +10,36 @@ class Rental {
     }
 
     public String getMovieTitle() {
-        return getMovie().getTitle();
+        return movie.getTitle();
     }
 
     public int getFrequentRenterPoints() {
         // add frequent renter points
         int frequentRenterPoints = 1;
         // add bonus for a two days new release rental
-        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE)
-                &&
-                getDaysRented() > 1)
-            frequentRenterPoints++;
+        if ((movie.getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) frequentRenterPoints++;
         return frequentRenterPoints;
     }
 
     public double getCharge() {
         double thisAmount = 0;
         //determine amounts for rental line
-        switch (getMovie().getPriceCode()) {
+        switch (movie.getPriceCode()) {
             case Movie.REGULAR:
                 thisAmount += 2;
-                if (getDaysRented() > 2)
-                    thisAmount += (getDaysRented() - 2) * 1.5;
+                if (daysRented > 2)
+                    thisAmount += (daysRented - 2) * 1.5;
                 break;
             case Movie.NEW_RELEASE:
-                thisAmount += getDaysRented() * 3;
+                thisAmount += daysRented * 3;
                 break;
             case Movie.CHILDREN:
                 thisAmount += 1.5;
-                if (getDaysRented() > 3)
-                    thisAmount += (getDaysRented() - 3) * 1.5;
+                if (daysRented > 3)
+                    thisAmount += (daysRented - 3) * 1.5;
                 break;
         }
         return thisAmount;
     }
 
-    public int getDaysRented() {
-        return daysRented;
-    }
-
-    public Movie getMovie() {
-        return movie;
-    }
 }

b9bc93a31ea5509258d89ca32506320bb087f018
zhangyi <brucezhang@tom.com>
2024-12-13 21:35:23 +0800

    yi - chapter 23 - complete the refactoring of Movie class

--- src/main/java/zhangyi/refactoring/mfbook/Movie.java
+++ src/main/java/zhangyi/refactoring/mfbook/Movie.java
@@ -1,16 +1,13 @@
 package zhangyi.refactoring.mfbook;
 
 public class Movie {
-    public static final int REGULAR = 0;
-    public static final int NEW_RELEASE = 1;
-    public static final int CHILDREN = 2;
     private final PriceCode priceCodeStrategy;
 
     private String title;
 
     public Movie(String title, int priceCode) {
         this.title = title;
-        priceCodeStrategy = getPriceCode(priceCode);
+        priceCodeStrategy = PriceCode.getPriceCode(priceCode);
     }
 
     public String getTitle() {
@@ -18,22 +15,7 @@ public class Movie {
     }
 
     public double getCharge(int daysRented) {
-        double thisAmount = 0;
-        thisAmount = priceCodeStrategy.amountFor(daysRented);
-        return thisAmount;
-    }
-
-    private PriceCode getPriceCode(int priceCode) {
-        switch (priceCode) {
-            case REGULAR:
-                return new RegularPriceCode();
-            case NEW_RELEASE:
-                return new NewReleasePriceCode();
-            case CHILDREN:
-                return new ChildrenPriceCode();
-            default:
-                return new DefaultPriceCode();
-        }
+        return priceCodeStrategy.amountFor(daysRented);
     }
 
     public int getFrequentRenterPoints(int daysRented) {
--- src/main/java/zhangyi/refactoring/mfbook/PriceCode.java
+++ src/main/java/zhangyi/refactoring/mfbook/PriceCode.java
@@ -1,6 +1,23 @@
 package zhangyi.refactoring.mfbook;
 
 public abstract class PriceCode {
+    public static final int REGULAR = 0;
+    public static final int NEW_RELEASE = 1;
+    public static final int CHILDREN = 2;
+
+    public static PriceCode getPriceCode(int priceCode) {
+        switch (priceCode) {
+            case REGULAR:
+                return new RegularPriceCode();
+            case NEW_RELEASE:
+                return new NewReleasePriceCode();
+            case CHILDREN:
+                return new ChildrenPriceCode();
+            default:
+                return new DefaultPriceCode();
+        }
+    }
+
     public abstract double amountFor(int daysRented);
 
     public int pointsFor(int daysRented) {

a2c6f26fdfcac6fc973dad4740da3ec0e61b7744
zhangyi <brucezhang@tom.com>
2025-01-19 17:17:34 +0800

    yi - chapter 26 - move methods to Movie by AI

--- src/main/java/zhangyi/refactoring/mfbook/Movie.java
+++ src/main/java/zhangyi/refactoring/mfbook/Movie.java
@@ -1,6 +1,6 @@
 package zhangyi.refactoring.mfbook;
 
-public class Movie {
+class Movie {
     public static final int REGULAR = 0;
     public static final int NEW_RELEASE = 1;
     public static final int CHILDREN = 2;
@@ -17,11 +17,39 @@ public class Movie {
         return priceCode;
     }
 
-    public void setPriceCode(int arg) {
-        priceCode = arg;
+    public void setPriceCode(int priceCode) {
+        this.priceCode = priceCode;
     }
 
     public String getTitle() {
         return title;
+    }
+
+    public double getAmount(int daysRented) {
+        double result = 0;
+        switch (priceCode) {
+            case REGULAR:
+                result += 2;
+                if (daysRented > 2)
+                    result += (daysRented - 2) * 1.5;
+                break;
+            case NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+            case CHILDREN:
+                result += 1.5;
+                if (daysRented > 3)
+                    result += (daysRented - 3) * 1.5;
+                break;
+        }
+        return result;
+    }
+
+    public int getFrequentRenterPoints(int daysRented) {
+        int points = 1;
+        // add bonus for a two days new release rental
+        if (priceCode == NEW_RELEASE && daysRented > 1)
+            points++;
+        return points;
     }
 }
--- src/main/java/zhangyi/refactoring/mfbook/Rental.java
+++ src/main/java/zhangyi/refactoring/mfbook/Rental.java
@@ -18,30 +18,10 @@ class Rental {
     }
 
     public double getAmount() {
-        double result = 0;
-        switch (movie.getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (daysRented > 2)
-                    result += (daysRented - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += daysRented * 3;
-                break;
-            case Movie.CHILDREN:
-                result += 1.5;
-                if (daysRented > 3)
-                    result += (daysRented - 3) * 1.5;
-                break;
-        }
-        return result;
+        return movie.getAmount(daysRented);
     }
 
     public int getFrequentRenterPoints() {
-        int points = 1;
-        // add bonus for a two day new release rental
-        if (movie.getPriceCode() == Movie.NEW_RELEASE && daysRented > 1)
-            points++;
-        return points;
+        return movie.getFrequentRenterPoints(daysRented);
     }
 }

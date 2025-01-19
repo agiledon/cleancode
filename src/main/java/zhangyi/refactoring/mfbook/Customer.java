4ff1495c658b169a02e8b80d151ef6226cc6f534
zhangyi <brucezhang@tom.com>
2025-01-19 17:10:22 +0800

    yi - chapter 26 - move methods to Rantal by AI

--- src/main/java/zhangyi/refactoring/mfbook/Customer.java
+++ src/main/java/zhangyi/refactoring/mfbook/Customer.java
@@ -25,47 +25,17 @@ class Customer {
         StringBuilder result = new StringBuilder("Rental Record for " + getName() + "\n");
 
         for (Rental each : rentals) {
-            double thisAmount = amountFor(each);
-
             // add frequent renter points
-            frequentRenterPoints += frequentRenterPointsFor(each);
+            frequentRenterPoints += each.getFrequentRenterPoints();
 
             // show figures for this rental
-            result.append("\t").append(each.getMovie().getTitle()).append("\t").append(thisAmount).append("\n");
-            totalAmount += thisAmount;
+            result.append("\t").append(each.getMovie().getTitle()).append("\t").append(each.getAmount()).append("\n");
+            totalAmount += each.getAmount();
         }
 
         // add footer lines
         result.append("Amount owed is ").append(totalAmount).append("\n");
         result.append("You earned ").append(frequentRenterPoints).append(" frequent renter points");
         return result.toString();
-    }
-
-    private double amountFor(Rental aRental) {
-        double result = 0;
-        switch (aRental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (aRental.getDaysRented() > 2)
-                    result += (aRental.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += aRental.getDaysRented() * 3;
-                break;
-            case Movie.CHILDREN:
-                result += 1.5;
-                if (aRental.getDaysRented() > 3)
-                    result += (aRental.getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return result;
-    }
-
-    private int frequentRenterPointsFor(Rental aRental) {
-        int points = 1;
-        // add bonus for a two day new release rental
-        if (aRental.getMovie().getPriceCode() == Movie.NEW_RELEASE && aRental.getDaysRented() > 1)
-            points++;
-        return points;
     }
 }
--- src/main/java/zhangyi/refactoring/mfbook/Rental.java
+++ src/main/java/zhangyi/refactoring/mfbook/Rental.java
@@ -9,11 +9,39 @@ class Rental {
         this.daysRented = daysRented;
     }
 
+    public Movie getMovie() {
+        return movie;
+    }
+
     public int getDaysRented() {
         return daysRented;
     }
 
-    public Movie getMovie() {
-        return movie;
+    public double getAmount() {
+        double result = 0;
+        switch (movie.getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (daysRented > 2)
+                    result += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+            case Movie.CHILDREN:
+                result += 1.5;
+                if (daysRented > 3)
+                    result += (daysRented - 3) * 1.5;
+                break;
+        }
+        return result;
+    }
+
+    public int getFrequentRenterPoints() {
+        int points = 1;
+        // add bonus for a two day new release rental
+        if (movie.getPriceCode() == Movie.NEW_RELEASE && daysRented > 1)
+            points++;
+        return points;
     }
 }

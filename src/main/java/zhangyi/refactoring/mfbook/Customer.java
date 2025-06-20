1e91bfca734e322e14d6ce4b1503367c8623e293
zhangyi <zhangyi1101@foxmail.com>
2025-06-20 17:17:36 +0800

    yi - refactor statement()

--- src/main/java/zhangyi/refactoring/mfbook/Customer.java
+++ src/main/java/zhangyi/refactoring/mfbook/Customer.java
@@ -20,44 +20,27 @@ class Customer {
     }
 
     public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
-        String result = "Rental Record for " + getName() + "\n";
+        StringBuilder result = new StringBuilder("Rental Record for " + getName() + "\n");
 
         for (Rental each : rentals) {
-            double thisAmount = 0;
-            //determine amounts for each line
-            switch (each.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (each.getDaysRented() > 2)
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
-                    break;
-                case Movie.CHILDREN:
-                    thisAmount += 1.5;
-                    if (each.getDaysRented() > 3)
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
-                    break;
-            }
-            // add frequent renter points
-            frequentRenterPoints++;
-            // add bonus for a two days new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-                    &&
-                    each.getDaysRented() > 1)
-                frequentRenterPoints++;
-            //show figures
-            result += "\t" + each.getMovie().getTitle() + "\t" + thisAmount + "\n";
-            totalAmount += thisAmount;
+            result.append("\t").append(each.getMovie().getTitle())
+                    .append("\t").append(each.getAmount()).append("\n");
         }
 
-        //add footer lines
-        result += "Amount owed is " + totalAmount + "\n";
-        result += "You earned " + frequentRenterPoints +
-                " frequent renter points";
-        return result;
+        result.append("Amount owed is ").append(getTotalAmount()).append("\n");
+        result.append("You earned ").append(getFrequentRenterPoints()).append(" frequent renter points");
+        return result.toString();
+    }
+
+    private double getTotalAmount() {
+        return rentals.stream()
+                .mapToDouble(Rental::getAmount)
+                .sum();
+    }
+
+    private int getFrequentRenterPoints() {
+        return rentals.stream()
+                .mapToInt(Rental::getFrequentRenterPoints)
+                .sum();
     }
 }
--- src/main/java/zhangyi/refactoring/mfbook/Rental.java
+++ src/main/java/zhangyi/refactoring/mfbook/Rental.java
@@ -16,4 +16,32 @@ class Rental {
     public Movie getMovie() {
         return movie;
     }
+
+    public double getAmount() {
+        double amount = 0;
+        switch (movie.getPriceCode()) {
+            case Movie.REGULAR:
+                amount += 2;
+                if (daysRented > 2)
+                    amount += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                amount += daysRented * 3;
+                break;
+            case Movie.CHILDREN:
+                amount += 1.5;
+                if (daysRented > 3)
+                    amount += (daysRented - 3) * 1.5;
+                break;
+        }
+        return amount;
+    }
+
+    public int getFrequentRenterPoints() {
+        int points = 1;
+        // add bonus for a two days new release rental
+        if ((movie.getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
+            points++;
+        return points;
+    }
 }
